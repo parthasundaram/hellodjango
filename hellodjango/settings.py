6af70a0848b68680b4eee53ed120c7941e968abe
Partha Sundaram <parthasundaram@gmail.com>
2015-01-21 23:07:04 -0800

    settings update

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -77,6 +77,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+UBER_CLIENT_ID = 'IIBtxGDpQ5qz3RiBkBhJXNlvSYcRAZMs'
+UBER_CLIENT_SECRET = 'YpqylrpkTJOgGmJIDS9TXN3m2f89Ek-bTkYIQXUi'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
